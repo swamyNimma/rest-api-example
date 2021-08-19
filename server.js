7a4ef5722b4925deb6499e0e0da71b3ed69cad19
swamyNimma <88778775+swamyNimma@users.noreply.github.com>
2021-08-19 13:42:22 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+//REST API demo in Node.js
+var express = require('express'); // requre the express framework
+var app = express();
+var fs = require('fs'); //require file system object
+
+// Endpoint to Get a list of users
+app.get('/getUsers', function(req, res){
+    fs.readFile(__dirname + "/" + "user.json", 'utf8', function(err, data){
+        console.log("res: "+data);
+        res.end(data); // you can also use res.send()
+    });
+}) 
+ 
+
+
+ // Create a server to listen at port 8080
+var server = app.listen(8080, function(){
+    var host = server.address().address
+    var port = server.address().port
+    console.log("REST API demo app listening at http://%s:%s", host, port)
+})
